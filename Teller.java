c1311cf0b13d630536b218ed848cc9ac796860fe
chandrairawan <chandrairawan32@gmail.com>
2016-03-12 11:05:32 +0700

    Modul 4
    String, StringBuffer, Math, Wrapper Classes

--- Teller.java
+++ Teller.java
@@ -1,16 +1,18 @@
 import java.util.*;
+import java.math.*;
+
 
 /**
  * JBank Program
  * Customer class
  * 
  * @author Abdul Chandra Irawan - 1306405244
- * @version 05.03.2016
+ * @version 11.03.2016
  */
 
 public class Teller
 {
-	/**
+    /**
      * Deklarasi variabel class :
      */
     //public Customer c1 = new Customer(); //Objek Customer dan menyimpannya dalam variabel c1
@@ -18,7 +20,10 @@ public class Teller
     //public String fullname; //objek nama costumer
     //public Account acct; //objek Account
     //public double Balance; //objek balance
-	
+    
+    //Modul 4
+    private static MathContext mc = new MathContext(8);
+    private static MathContext mc1 = new MathContext(3);
     
     /**
      * Main Method in Teller Class
@@ -28,10 +33,13 @@ public class Teller
         String user_c;
         String nama_depan;
         String nama_belakang;
-        String dob;
+        Date dob;
+        //Date startTime;
+        //Date closeTime;
         String no_telp;
         String acct_type;
         int bal;
+        int year=0, month=0, day=0;
 
         
         Scanner scan = new Scanner(System.in);
@@ -47,17 +55,26 @@ public class Teller
             int i = 0;
             do{
                 //input customer's data
-                System.out.println("Masukkan Nama Depan Anda   :");
+                System.out.println("Masukkan Nama Depan Anda :");
                 nama_depan = scan.nextLine();
-                System.out.println("Masukkan Nama Belakang Anda:");
+                System.out.println("Masukkan Nama Belakang Anda :");
                 nama_belakang = scan.nextLine();
-                System.out.println("Masukkan Tgl. Lahir Anda   :");
-                dob = scan.nextLine();
-                System.out.println("Masukkan No. Telepon Anda  :");
+                System.out.println("Masukan Tgl. Lahir Anda :");
+                //dob = scan.nextLine();
+                System.out.println("Masukkan No. Telepon Anda :");
                 no_telp = scan.nextLine();
                 
+                Calendar start = new GregorianCalendar();
+                start.set(Calendar.HOUR_OF_DAY,8);
+                start.set(Calendar.MINUTE, 0);
+                Date startTime = start.getTime();
+                Calendar close = new GregorianCalendar();
+                close.set(Calendar.HOUR_OF_DAY,17);
+                close.set(Calendar.MINUTE, 0);
+                Date closeTime = close.getTime();
+                
                 //create customer's object
-                c[i] = new Customer(nama_depan, nama_belakang, dob);
+                c[i] = new Customer(nama_depan, nama_belakang, (new GregorianCalendar(year, month, day).getTime()));
                 c[i].setPhoneNumber(no_telp);
                 
                 //create account object
@@ -86,12 +103,18 @@ public class Teller
                 else{
                     a[i] = new Account();
                 }
+                
+                
                //Menampilkan informasi akun
+                System.out.print("Date    : "+c[i].getDateOfBirth());
+                System.out.println();
+               
                 System.out.println("Nama    : "+c[i].getName());
                 System.out.println("Phone   : "+c[i].getPhoneNumber());
                 System.out.println("ID      : "+c[i].getCustId());
                 System.out.println("Balance : "+a[i].getBalance());
                 System.out.println("Account : "+a[i].getAcctType());
+                System.out.println(startTime+" TO "+closeTime);
                 
                 //Ingin membuat customer lagi atau tidak
                 System.out.println("Apakah ingin membuat customer lagi? Ya/Tidak");
@@ -107,8 +130,8 @@ public class Teller
             System.out.println("Finish. Terima Kasih");
         }
     }
-	
-	/**
+    
+    /**
      * Constructor untuk objek Teller
      */
     public Teller(){
@@ -126,5 +149,48 @@ public class Teller
          
          System.out.println(Balance);
          */
+        
+        
+        //Objek Account Saving, Invest, dan CreditLine 
+        Account saving = new Account ('S', 1000);
+        Account invest = new Account ('I', 1000);
+        Account creditline = new Account ('L', 500);
+        
+        
+        BigDecimal balanceS = new BigDecimal (saving.getBalance());
+        BigDecimal balanceI = new BigDecimal (invest.getBalance());
+        BigDecimal balanceL = new BigDecimal (250);
+        
+        //Saving Interest Rates 3% setiap tahun, dihitung setiap hari
+        BigDecimal rS = new BigDecimal (.03); 
+        //Investment Interest Rates 7% jika Investment Termnya mencapai 12 bulan
+        BigDecimal rI = new BigDecimal (.07);
+        //LOC Interest Rates 18%, dihitung setiap hari 
+        BigDecimal rL = new BigDecimal (.18);
+        
+        BigDecimal n = new BigDecimal (360); //compound harian
+        BigDecimal t = new BigDecimal (1.0); //untuk 1 tahun
+        
+        BigDecimal f1S = rS.divide(n, mc).add(new BigDecimal(1.0));
+        BigDecimal f1I = rI.divide(n, mc).add(new BigDecimal(1.0));
+        BigDecimal f1L = rL.divide(n, mc).add(new BigDecimal(1.0));
+        
+        BigDecimal f2 = n.multiply(t, mc);
+        
+        BigDecimal f3S = new BigDecimal (Math.pow(f1S.doubleValue(), f2.doubleValue()),mc);
+        BigDecimal f3I = new BigDecimal (Math.pow(f1I.doubleValue(), f2.doubleValue()),mc);
+        BigDecimal f3L = new BigDecimal (Math.pow(f1L.doubleValue(), f2.doubleValue()),mc);
+        
+        
+        BigDecimal f4S = f3S.multiply(balanceS, mc);
+        BigDecimal f4I = f3I.multiply(balanceI, mc);
+        BigDecimal f4L = f3L.multiply(balanceI, mc);
+        
+        //Menampilkan Balance
+        System.out.println("Balance Saving : " +saving.getBalance());
+        System.out.println("Balance Investment : "+invest.getBalance());        
+        System.out.println("Balance Saving : " +f4S.doubleValue());
+        System.out.println("Balance Investment : " +f4I.doubleValue());
+        System.out.println("Balance Credit : " +f4L.doubleValue());
     }
 }
